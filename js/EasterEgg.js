13ede84a9e4442331b6e5348013b468a2e9a7743
Dustin Jiang <56217843+Dustin-Jiang@users.noreply.github.com>
2020-11-08 15:48:13 +0800

    Fixed EasterEgg.js

--- js/EasterEgg.js
+++ js/EasterEgg.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   date = new Date();
-  if (date.getDate() == 11 && date.getMonth() == 11) {
-    $(".psgContainer:nth-child(1) .classImg").src("https://cdn.jsdelivr.net/gh/Dustin-Jiang/D2308MainPageCode@master/img/MainPage/EasterEgg.png");
+  if (date.getDate() == 11 && date.getMonth() == 10) {
+    $("#warp > table > tbody > tr > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(1) > td > table > tbody > tr > td > div:nth-child(9) .classImg")[0].src = "https://cdn.jsdelivr.net/gh/Dustin-Jiang/D2308MainPageCode@master/img/MainPage/EasterEgg.png";
   }
 })
