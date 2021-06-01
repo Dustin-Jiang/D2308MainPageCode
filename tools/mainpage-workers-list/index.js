e8b2e8c02097d0452d06fe5faf6f350feedaecde
Dustin Jiang <dustinjiang@outlook.com>
2021-06-01 17:44:48 +0800

    [Content] Fix ceil to floor to get correct week number

--- tools/mainpage-workers-list/index.js
+++ tools/mainpage-workers-list/index.js
@@ -27,13 +27,11 @@ function getYearWeek(endDate) {
   var beginDate = new Date(endDate.getFullYear(), 0, 1);
   //星期从0-6,0代表星期天，6代表星期六
   var endWeek = endDate.getDay();
-  if (endWeek == 0) endWeek = 7;
   var beginWeek = beginDate.getDay();
-  if (beginWeek == 0) beginWeek = 7;
   //计算两个日期的天数差
   var millisDiff = endDate.getTime() - beginDate.getTime();
-  var dayDiff = Math.floor((millisDiff + (beginWeek - endWeek) * (24 * 60 * 60 * 1000)) / 86400000);
-  return Math.ceil((dayDiff  + 1)/ 7); //Plus 1 to adapt Sunday-start week
+  var dayDiff = Math.floor((millisDiff + beginWeek * (24 * 60 * 60 * 1000)) / 86400000);
+  return Math.floor(dayDiff / 7); //Plus 1 to adapt
 }
 
 $(document).ready(function () {
