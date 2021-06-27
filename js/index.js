1ba97955464924ec6d29be588f4e8c0f615fcdd4
Dustin Jiang <dustinjiang@outlook.com>
2021-06-27 15:55:28 +0800

    [Content] Add code for homepage luckness viewer

--- js/index.js
+++ js/index.js
@@ -1,111 +1,13 @@
-/*
- * index.js
- *
- * Libraries in use:
- *
- * async-script-loader:
- *  https://github.com/CSharperMantle/async-script-loader
- *  MIT License. Copyright (c) Rong "Mantle" Bao (aka CSharperMantle)
- * 
- * lodash:
- *  https://github.com/lodash/lodash/
- *  MIT License. Copyright JS Foundation and other contributors <https://js.foundation/>
- *
- * jquery:
- *  https://github.com/jquery/jquery/
- *  MIT Licentse. 
- * 
- * popper.js:
- * 
- * twitter-bootstrap: 
- * 
- * 
- */
-
-(function () {
-  const AsyncScriptLoader = {
-    loadScript: function (
-      url,
-      baseElem,
-      resolveWhen,
-      isWithIntegrity,
-      integrity,
-      crossorigin
-    ) {
-      return new Promise(function (resolve, reject) {
-        const scriptElem = document.createElement('script');
-        scriptElem.setAttribute('src', url);
-        if (isWithIntegrity) {
-          scriptElem.setAttribute('integrity', integrity);
-          scriptElem.setAttribute('crossorigin', crossorigin);
-        }
-        scriptElem.addEventListener('load', function () {
-          while (!resolveWhen());
-          resolve();
-        });
-        scriptElem.addEventListener('error', function () {
-          reject(new Error(`AsyncScriptLoader: ${url} fails to load`));
-        });
-        baseElem.insertAdjacentElement('afterbegin', scriptElem);
-      });
-    }
-  };
-  window.AsyncScriptLoader = AsyncScriptLoader;
-})();
-(function () {
-  const body = document.getElementsByTagName('body')[0];
-
-  AsyncScriptLoader.loadScript(
-    'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.20/lodash.min.js',
-    body,
-    () => {
-      return typeof _ === 'function';
-    },
-    true,
-    'sha512-90vH1Z83AJY9DmlWa8WkjkV79yfS2n2Oxhsi2dZbIv0nC4E6m5AbH8Nh156kkM7JePmqD6tcZsfad1ueoaovww==',
-    'anonymous'
-  ).then(() => {
-    return AsyncScriptLoader.loadScript(
-      'https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js',
-      body,
-      () => {
-        return typeof $ !== 'undefined';
-      },
-      false,
-      null,
-      null
-    );
-  }).then(() => {
-    return AsyncScriptLoader.loadScript(
-      'https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js',
-      body,
-      () => {
-        return typeof Popper !== 'undefined';
-      },
-      false,
-      null,
-      null
-    ).then(() => {
-      'https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js',
-        body,
-        () => {
-          return typeof _createClass !== 'undefined';
-        },
-        false,
-        null,
-        null;
-    }).then(() => {
-      initialize();
-    }).catch((reason) => {
-      console.log(reason);
-    });
-  });
-})();
-
-function loadHTML(){
-  return 0;
-}
-
-function initialize(){
-  loadHTML();
-}
+$(document).ready(function () {
+  var elemContainer = document.getElementById("dynapage-container");
+  var jsDeps = [
+    'https://cdn.jsdelivr.net/gh/Dustin-Jiang/D2308MainPageCode@HEAD/tools/mainpage-workers-list/index.js',
+    'https://cdn.jsdelivr.net/gh/Dustin-Jiang/D2308MainPageCode@HEAD/tools/homepage-luckness-viewer/script.js'
+  ];
+  for (var idx = 0; idx < jsDeps.length; idx++) {
+    var dep = jsDeps[idx];
+    var elemJs = document.createElement("script");
+    elemJs.setAttribute("src", dep);
+    elemContainer.insertAdjacentElement("beforeend", elemJs);
+  }
+});
---
+++ tools/homepage-luckness-viewer/script.js
@@ -0,0 +1,325 @@
+fortunes = [
+  {
+    "activity": "考试",
+    "good": "考的都会 蒙的全对",
+    "bad": "考点细如金针菇"
+  },
+  {
+    "activity": "重测",
+    "good": "重测一次满分",
+    "bad": "重测成绩还不如之前"
+  },
+  {
+    "activity": "写作业",
+    "good": "晚自修前就写完",
+    "bad": "写了也写不完"
+  },
+  {
+    "activity": "不交作业",
+    "good": "老师不会发现的",
+    "bad": "试试就逝世"
+  },
+  {
+    "activity": "补作业",
+    "good": "肯定补的完",
+    "bad": "肯定补不完"
+  },
+  {
+    "activity": "复习",
+    "good": "背了都考中",
+    "bad": "背了也没用"
+  },
+  {
+    "activity": "预习",
+    "good": "上课睡觉也没事",
+    "bad": "上课只配睡觉"
+  },
+  {
+    "activity": "听课",
+    "good": "听了全会",
+    "bad": "听了也不会"
+  },
+  {
+    "activity": "上课睡觉",
+    "good": "不听也学得会",
+    "bad": "听了也学不会"
+  },
+  {
+    "activity": "内卷",
+    "good": "你就是卷王",
+    "bad": "卷王一定是王后雄"
+  },
+  {
+    "activity": "刷题",
+    "good": "刷的题都考的到",
+    "bad": "刷一题错一题"
+  },
+  {
+    "activity": "晚自修睡觉",
+    "good": "作业晚自修前就能写完",
+    "bad": "作业写不完了"
+  },
+  {
+    "activity": "晚自修写作业",
+    "good": "不写作业真的写不完了",
+    "bad": "晚自修前就写好了"
+  },
+  {
+    "activity": "晚自修看小说",
+    "good": "作业晚自修前就能写完",
+    "bad": "作业写不完了"
+  },
+  {
+    "activity": "图书馆写作业",
+    "good": "不写作业真的写不完了",
+    "bad": "晚自修前就写好了"
+  },
+  {
+    "activity": "图书馆看小说",
+    "good": "作业晚自修前就能写完",
+    "bad": "作业写不完了"
+  },
+  {
+    "activity": "图书馆睡觉",
+    "good": "作业晚自修前就能写完",
+    "bad": "作业写不完了"
+  },
+  {
+    "activity": "逛网吧",
+    "good": "没人来抓你",
+    "bad": "保证被抓住"
+  },
+  {
+    "activity": "做值日",
+    "good": "不然要被扣分",
+    "bad": "做了也被扣分"
+  },
+  {
+    "activity": "不做值日",
+    "good": "Dustbin抓不住你",
+    "bad": "不然要被扣分"
+  },
+  {
+    "activity": "去办公室提问",
+    "good": "老师全闲着",
+    "bad": "老师忙的很"
+  },
+  {
+    "activity": "去办公室喝茶",
+    "good": "很快就能出来了",
+    "bad": "很快就能抬着出来了"
+  },
+  {
+    "activity": "去办公室对线",
+    "good": "老师说不过你",
+    "bad": "你太嫩了"
+  },
+  {
+    "activity": "上课迟到",
+    "good": "老师来的比你晚",
+    "bad": "迟到要表演"
+  },
+  {
+    "activity": "作业晚交",
+    "good": "老师不会知道的",
+    "bad": "老师刚查谁没交"
+  },
+  {
+    "activity": "剁手",
+    "good": "历史最低价",
+    "bad": "明天就掉价"
+  },
+  {
+    "activity": "抢购",
+    "good": "肯定抢得到",
+    "bad": "一定抢不到"
+  },
+  {
+    "activity": "下单",
+    "good": "明天就送到",
+    "bad": "快递一礼拜"
+  },
+  {
+    "activity": "收快递",
+    "good": "包装完好",
+    "bad": "快递盒伊拉克成色"
+  },
+  {
+    "activity": "退货",
+    "good": "商家包邮费",
+    "bad": "商家不给退"
+  },
+  {
+    "activity": "等等再买",
+    "good": "明天就降价",
+    "bad": "明天就没货"
+  },
+  {
+    "activity": "不买立省百分百",
+    "good": "你用不到的",
+    "bad": "早买早享受"
+  },
+  {
+    "activity": "种草",
+    "good": "明天就开售",
+    "bad": "多一个购物车钉子户"
+  },
+  {
+    "activity": "吃饭",
+    "good": "便宜吃得好",
+    "bad": "又贵还血亏"
+  },
+  {
+    "activity": "吃肉末蒸蛋",
+    "good": "大家都喜欢",
+    "bad": "里面有蛋壳"
+  },
+  {
+    "activity": "吃水煮肉片",
+    "good": "肉片超级多",
+    "bad": "就是水煮豆芽"
+  },
+  {
+    "activity": "吃酸菜鱼",
+    "good": "鱼片还不少",
+    "bad": "就是鱼味酸菜"
+  },
+  {
+    "activity": "吃小卖部",
+    "good": "食堂排队排不到",
+    "bad": "食堂菜超好"
+  },
+  {
+    "activity": "去课间物语",
+    "good": "排队排的到",
+    "bad": "又要去甲醛"
+  },
+  {
+    "activity": "买铁板烧",
+    "good": "排队排的到",
+    "bad": "排队排不到"
+  },
+  {
+    "activity": "买汉堡",
+    "good": "快点吃好去内卷",
+    "bad": "排队排不到"
+  },
+  {
+    "activity": "吃鸡肉卷",
+    "good": "快点吃好去内卷",
+    "bad": "排队排不到"
+  },
+  {
+    "activity": "吃煲仔饭",
+    "good": "今天真香",
+    "bad": "早卖完了"
+  },
+  {
+    "activity": "吃白宰鸡",
+    "good": "鸡肉没骨头",
+    "bad": "酱油没咸味"
+  },
+  {
+    "activity": "吃烤鸭",
+    "good": "鸭肉烤的真香",
+    "bad": "只有色没有味"
+  },
+  {
+    "activity": "打篮球",
+    "good": "乱投篮都能进",
+    "bad": "随便打都吃萝卜干"
+  },
+  {
+    "activity": "打乒乓",
+    "good": "刀削面玩的嗨",
+    "bad": "球球不上台"
+  },
+  {
+    "activity": "打排球",
+    "good": "乱接球都过网",
+    "bad": "手腕都打红了"
+  },
+  {
+    "activity": "打羽毛球",
+    "good": "大力出奇迹",
+    "bad": "球球不过网"
+  },
+  {
+    "activity": "踢足球",
+    "good": "球球进空门",
+    "bad": "守门黄油手"
+  }
+];
+
+$(document).ready(function(){
+  const pageSrc = 'https://cdn.jsdelivr.net/gh/Dustin-Jiang/D2308MainPageCode@HEAD/tools/mainpage-workers-list/index.html';
+
+  var htmlXhr = new XMLHttpRequest();
+  htmlXhr.addEventListener("load", function () {
+    if (htmlXhr.status === 200) {
+      var element = $("#homepage-luckness-viewer")[0];
+      element.innerHTML = htmlXhr.responseText;
+
+      htmlXhr.open('GET', pageSrc);
+      htmlXhr.send();
+      var d = new Date();
+      var n = '';
+      n += d.getFullYear();
+      if ((d.getMonth() + 1) < 10) {
+        n += '0';
+      }
+      n += d.getMonth() + 1;
+      n += d.getDate();
+
+      n = rand(n);
+      console.log(n);
+      if (n < 0.15) {
+        //大凶
+        $(".luckness.main")[0].innerText = "大凶";
+      } else if (n < 0.4) {
+        //小凶
+        $(".luckness.main")[0].innerText = "小凶";
+        randomFortune(n);
+      } else if (n < 0.6) {
+        //中平
+        $(".luckness.main")[0].innerText = "中平";
+        randomFortune(n);
+      } else if (n < 0.85) {
+        //小吉
+        $(".luckness.main")[0].innerText = "小吉";
+        randomFortune(n);
+      } else {
+        //大吉
+        $(".luckness.main")[0].innerText = "大吉";
+      }
+    }
+  });
+  htmlXhr.open('GET', pageSrc);
+  htmlXhr.send();
+});
+
+function rand(seed){
+  n = seed * seed;
+  p = n % 10000000;
+  a = Math.floor(p / 10000);
+  q = n * n % 10000;
+  b = Math.floor(q / 10);
+  if (a > b) return b / a;
+  else return a / b;
+};
+
+function randomFortune(n) {
+  index = [];
+  for(i=0;i<6;i++) {
+    index.push(Math.ceil(fortunes.length * n));
+    n = rand(Math.floor(n * 1000000));
+  }
+  for(i in index){
+    $(".divide li")[i].innerText = fortunes[i]["activity"];
+    if(i < 3) {
+      $(".divide .saying")[i].innerText = fortunes[i]["good"];
+    } else {
+      $(".divide .saying")[i].innerText = fortunes[i]["bad"];
+    }
+  }
+}
