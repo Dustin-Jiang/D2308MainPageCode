82ab2addb2d1d96755b7d3d648702cd10251cd82
Dustin Jiang <dustinjiang@outlook.com>
2021-10-10 12:26:14 +0800

    [Content] Add file to fk wxm

---
+++ js/headlineEasterEgg.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    clickTime = 5
+    timeout = 500
+    click = 0
+    $("#headline-easter-egg")[0].onclick = function() {
+        click += 1;
+        if (click >= clickTime) {
+            alert()
+        }
+        setTimeout(function() {
+            click = 0
+        }, timeout * (click + 1))
+    }
+})
+
+function displayEasterEgg() {
+    model = $(".model-hidden")[0]
+    model.classList.add("model");
+    model.classList.remove("model-hidden");
+    model.onclick = function() {
+        model = $(".model")[0]
+        model.classList.add("model-hidden");
+        model.classList.remove("model")
+    }
+}
